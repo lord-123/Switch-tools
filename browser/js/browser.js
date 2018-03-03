633caed4c2656f3d00db830df1feac139ddf8fdf
Mabel <Mattabel2002@gmail.com>
2018-03-03 11:24:34 +0000

    Remove browser from this repo to move into it's own repo

--- browser/js/browser.js
+++
@@ -1,9 +0,0 @@
-var window = document.getElementById("window");
-var navBar = document.getElementsByName("navBar");
-var url = "scorehero.com";
-
-function navigate() {
-    window.data = "keku";
-}
-
-navigate();
